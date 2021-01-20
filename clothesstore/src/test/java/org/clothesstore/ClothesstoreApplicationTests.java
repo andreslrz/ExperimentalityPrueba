e83766f91f0bb3eb33362b4e9a6d99a23f7e92c5
carllora <calopez@personalsoft.com.co>
2021-01-19 21:21:48 -0500

    Commit inicial

---
+++ clothesstore/src/test/java/org/clothesstore/ClothesstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clothesstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
